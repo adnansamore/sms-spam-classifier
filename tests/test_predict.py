06fa7f8cb4130e7f728d7735b2ec44c3ebc7384e
nbdk4l <nbdk4l@inf.elte.hu>
2025-08-28 16:57:15 +0200

    fix issues

--- tests/test_predict.py
+++ tests/test_predict.py
@@ -1,6 +1,9 @@
 import joblib
 from pathlib import Path
 
+
+labels = {0: "ham", 1: "spam"}
+
 def test_model_exists():
     """Check if trained model file exists."""
     model_path = Path("spam_model.joblib")
@@ -9,13 +12,15 @@ def test_model_exists():
 def test_prediction_spam():
     """Check if spammy message is predicted as spam."""
     model = joblib.load("spam_model.joblib")
-    sample_msg = ["Congratulations! You have won free tickets, claim now!"]
-    pred = model.predict(sample_msg)[0]
+    sample_msg = ["Congratulations! You have hunted internship, click link now!"]
+    pred_num = model.predict(sample_msg)[0]
+    pred = labels[pred_num]
     assert pred == "spam", f"Expected spam but got {pred}"
 
 def test_prediction_ham():
     """Check if normal message is predicted as ham."""
     model = joblib.load("spam_model.joblib")
-    sample_msg = ["Hey, are we still meeting at 5 pm today?"]
-    pred = model.predict(sample_msg)[0]
+    sample_msg = ["Hey, are we still meeting at 5 pm today near ELTE?"]
+    pred_num = model.predict(sample_msg)[0]
+    pred = labels[pred_num]
     assert pred == "ham", f"Expected ham but got {pred}"
